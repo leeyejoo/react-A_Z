e4d0bb32e05472fc4ffb2426a73ba270d90b70c5
leeyejoo <coolyejoo@naver.com>
2023-06-16 19:14:52 +0900

    넷플릭스 - 영화 Row 생성 및 배너 css 수정

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import requests from './api/requests';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner />
+      
+      <Row 
+        title="오직 넷플릭스에서"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="지금 뜨는 콘텐츠" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="평단의 찬사를 받은 영화" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="액션 영화" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="코미디 영화" id="CM" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="공포 영화" id="HM" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="로맨스 영화" id="RM" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   );
 }
---
+++ react-netflix-clone/src/components/Row.js
@@ -0,0 +1,51 @@
+import React, {useEffect,useState} from 'react';
+import axios from "../api/axios";
+import "./Row.css";
+
+export default function Row({isLargeRow, title, id, fetchUrl}) {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+  
+  //async, await : 비동기 요청 시 사용.. 
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    console.log('request', request);
+    console.log('requestResult',request.data.results)
+    setMovies(request.data.results);
+  }
+   
+  return (
+    <section className='row'>
+      {/* props로 가져온 title */}
+      <h2>{title}</h2> 
+      <div className='slider'>
+
+        <div className='slider_arrow-left'>
+          <span className='arrow'>{"<"}</span> {/* 중괄호 없이 < 만 쓰면 태그와 구분할 수 없어 에러 발생 */}
+        </div>
+
+        {/* 각 영화들  */}
+        <div className='row_posters' id={id}>
+          {movies.map(movie => (
+            <img
+              key={movie.id}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          ))}
+
+        </div>
+
+        <div className='slider_arrow-right'>
+          <span className='arrow'>{">"}</span> {/* 중괄호 없이 > 만 쓰면 태그와 구분할 수 없어 에러 발생 */}
+        </div>
+
+      </div>
+    </section>
+  )
+}
